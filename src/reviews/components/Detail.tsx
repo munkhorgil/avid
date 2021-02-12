c84d6e0af0f88cbc33fea91c93ce609da4963b23
Munkh-Orgil <munkhorgil@live.com>
2021-02-12 15:00:34 +0800

    little refactor

--- src/reviews/components/Detail.tsx
+++ src/reviews/components/Detail.tsx
@@ -10,22 +10,57 @@ type Props = {
 };
 
 function Detail({ review = {} as Review, history, isLoading }: Props) {
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   const handleBack = () => history.goBack();
 
   function renderRow(label: string, value: string | number) {
     return (
       <Table.Row>
-        <Table.TextCell>
-          {label}
-        </Table.TextCell>
-        <Table.TextCell width={600}>
-          {value}
-        </Table.TextCell>
+        <Table.TextCell>{label}</Table.TextCell>
+        <Table.TextCell width={600}>{value}</Table.TextCell>
       </Table.Row>
+    );
+  }
+
+  function renderContent() {
+    if (isLoading) {
+      return <Spinner />;
+    }
+
+    return (
+      <>
+        <BackButton onClick={handleBack} margin={16} />
+        <Table>
+          <Table.Head>
+            <Table.TextHeaderCell>Description</Table.TextHeaderCell>
+            <Table.TextHeaderCell>Value</Table.TextHeaderCell>
+          </Table.Head>
+          <Table.Body>
+            {renderRow("Title", review.review_title)}
+            {renderRow("Profile name", review.profile_name)}
+            {renderRow("Product company", review.product_company)}
+            {renderRow("Product", review.product)}
+            {renderRow("Review", review.review_text)}
+            {renderRow("Date", review.reviewed_at)}
+            {renderRow("Country", review.review_country)}
+            {renderRow("Total comments", review.total_comments)}
+            {renderRow("Helpful count", review.helpful_count)}
+            <Table.Row>
+              <Table.TextCell>URL</Table.TextCell>
+              <Table.TextCell>
+                <a href={review.url} target="_blank" rel="noreferrer">
+                  Click here
+                </a>
+              </Table.TextCell>
+            </Table.Row>
+            <Table.Row>
+              <Table.TextCell>Rating</Table.TextCell>
+              <Table.HeaderCell>
+                {generateRatingIcons(review.review_rating)}
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </>
     );
   }
 
@@ -37,38 +72,7 @@ function Detail({ review = {} as Review, history, isLoading }: Props) {
       border={false}
       flexDirection="column"
     >
-      <BackButton onClick={handleBack} margin={16} />
-      <Table>
-        <Table.Head>
-          <Table.TextHeaderCell>Description</Table.TextHeaderCell>
-          <Table.TextHeaderCell>Value</Table.TextHeaderCell>
-        </Table.Head>
-        <Table.Body>
-          {renderRow('Title', review.review_title)}
-          {renderRow('Profile name', review.profile_name)}
-          {renderRow('Product company', review.product_company)}
-          {renderRow('Product', review.product)}
-          {renderRow('Review', review.review_text)}
-          {renderRow('Date', review.reviewed_at)}
-          {renderRow('Country', review.review_country)}
-          {renderRow('Total comments', review.total_comments)}
-          {renderRow('Helpful count', review.helpful_count)}
-          <Table.Row>
-            <Table.TextCell>URL</Table.TextCell>
-            <Table.TextCell>
-              <a href={review.url} target="_blank" rel="noreferrer">
-                Click here
-              </a>
-            </Table.TextCell>
-          </Table.Row>
-          <Table.Row>
-            <Table.TextCell>Rating</Table.TextCell>
-            <Table.HeaderCell>
-              {generateRatingIcons(review.review_rating)}
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+      {renderContent()}
     </Pane>
   );
 }
